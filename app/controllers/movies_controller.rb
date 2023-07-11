675b0d3faa1c520354fd2a731fc74a030da966a7
Yazeed <dee3ayahmed25@gmail.com>
2023-07-11 15:40:16 +0300

    assignment completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,16 @@
 class MoviesController < ApplicationController
-  
+
+  def create
+     movie = Movi.create(movie_params)
+  render json: movie, status: :created
+  end
+
+  private
+
+def movie_params
+  params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+end
+
   def index
     movies = Movie.all
     render json: movies
